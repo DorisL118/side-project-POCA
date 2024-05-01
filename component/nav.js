ceccf9edf2e97e132a59fe434706a4dc107504fb
DorisL118 <c5090017@gmail.com>
2024-05-01 17:18:55 +0800

    add=-5-1

--- component/nav.js
+++ component/nav.js
@@ -1,11 +1,18 @@
-// 找到你的 .nav-item-2 元素
-const navItem2 = document.querySelector('.nav-item-2');
-// 找到你的 .overlay 元素
-const overlay = document.querySelector('.overlay');
+// const navItem2 = document.querySelector('.nav-item-2');
+// const overlay = document.querySelector('.overlay');
 
-// 當 .nav-item-2 被點擊時，觸發事件
-navItem2.addEventListener('click', function(){
-    // 切換 .show-overlay 類的存在，以顯示/隱藏毛玻璃效果
-    overlay.classList.toggle('show-overlay');
-    
-});
+// // 當 .nav-item-2 被點擊時，觸發事件
+// navItem2.addEventListener('click', function() {
+//     // 切換 .show-overlay 類的存在，以顯示/隱藏毛玻璃效果
+//     overlay.classList.toggle('show-overlay');
+// });
+
+// $(document).ready(function(){
+//     $(".nav-item-2").click(function(){
+//         // $(".overlay").fadeIn();
+//         console.log("nav-click");
+//     });
+// });
+
+
+
